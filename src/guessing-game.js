0b773f2394edcef76f529793f3f1d6ffa2f46942
konstantos <sunnyday-23@mail.ru>
2022-08-14 19:09:31 +0300

    feat: add task solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,20 @@
 class GuessingGame {
-    constructor() {}
-
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.ans = Math.round((this.min + this.max) / 2);
+        return this.ans;
     }
 
     lower() {
-
+        this.max = this.ans;
     }
 
     greater() {
-
+        this.min = this.ans;
     }
 }
 
